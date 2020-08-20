7033de4d44d564106af65f19a468b67189b925f1
jusufnathanael <jusuf.n@gmail.com>
2020-08-20 21:42:11 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,14 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
+        String line = "____________________________________________________________";
+        String greet = "Hello! I'm Duke\n" +
+                "What can I do for you?";
+        String bye = "Bye. Hope to see you again soon!";
+
         System.out.println("Hello from\n" + logo);
+        System.out.println(greet + "\n" + line);
+        System.out.println(bye);
     }
 }
