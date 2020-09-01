c0905fb9f5e19afa519054712149e6ff9fc32b53
jusufnathanael <jusuf.n@gmail.com>
2020-09-01 18:46:45 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -25,38 +25,75 @@ public class Duke {
         printLine();
     }
 
+    public static void acknowledgement(Task message, int i){
+        System.out.println("Got it. I've added this task:");
+        System.out.println("    " + message);
+        System.out.println("Now you have " + i + (i == 1 ? " task" : " tasks") + " in your list.");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-
         greet();
+
         Scanner in = new Scanner(System.in);
         String line = in.nextLine();
 
         Task[] t = new Task[100];
-
         int index = 0;
 
+
+        // main loop
         while (!line.equals("bye")){
 
             printLine();
+
+            // list
             if (line.equals("list")){
                 for (int i = 0; i < index; i++){
-                    t[i].printTask(i);
+                    System.out.print(i+1 + ". ");
+                    System.out.println(t[i]);
                 }
             }
+
+            // mark as done
             else if (line.startsWith("done")){
                 int l = Integer.parseInt(line.substring(5, line.length())) - 1;
                 t[l].markAsDone();
             }
+
+            // deadline
+            else if (line.startsWith("deadline")){
+                t[index] = new Deadline(line.substring(9, line.indexOf(" /")),
+                        line.substring(line.indexOf("/by") + 4));
+                acknowledgement(t[index], index+1);
+                index++;
+            }
+
+            // todo
+            else if (line.startsWith("todo")){
+                t[index] = new Todo(line.substring(5));
+                acknowledgement(t[index], index+1);
+                index++;
+            }
+
+            // event
+            else if (line.startsWith("event")){
+                t[index] = new Event(line.substring(6, line.indexOf(" /")),
+                        line.substring(line.indexOf("/at") + 4));
+                acknowledgement(t[index], index+1);
+                index++;
+            }
+
             else {
                 t[index] = new Task(line);
                 index++;
                 System.out.println("added: " + line);
             }
+
             printLine();
 
             line = in.nextLine();
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -17,8 +17,7 @@ public class Task {
         System.out.println("Nice! I've marked this task as done:");
         System.out.println("[" + getStatusIcon() + "] " + this.description);
     }
-
-    public void printTask(int i){
-        System.out.println(i+1 + ". [" + getStatusIcon() + "] " + this.description);
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
     }
 }
