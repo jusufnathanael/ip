51ded123f8725777435cc355d763fc3a5ba855ea
jusufnathanael <jusuf.n@gmail.com>
2020-09-08 21:50:30 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,10 +3,16 @@ import java.util.Scanner;
 public class Duke {
 
     public static void printLine(){
-        System.out.println("--------------------------------------------------");
+        System.out.println("------------------------------------------------------" +
+                "--------------------------");
     }
 
     public static void greet(){
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
         printLine();
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
         printLine();
@@ -24,6 +30,52 @@ public class Duke {
         printLine();
     }
 
+    public static int addTask(Task[] t, String message, int index) throws DukeException {
+
+        // list
+        if (message.equals("list")){
+            for (int i = 0; i < index; i++){
+                System.out.print(i+1 + ". ");
+                System.out.println(t[i]);
+            }
+        }
+
+        // mark as done
+        else if (message.startsWith("done")){
+            int l = Integer.parseInt(message.substring(5)) - 1;
+            t[l].markAsDone();
+        }
+
+        // deadline
+        else if (message.startsWith("deadline")){
+            t[index] = new Deadline(message.substring(9, message.indexOf(" /")),
+                    message.substring(message.indexOf("/by") + 4));
+            acknowledgement(t[index], index+1);
+            index++;
+        }
+
+        // todo
+        else if (message.startsWith("todo")){
+            t[index] = new Todo(message.substring(5));
+            acknowledgement(t[index], index+1);
+            index++;
+        }
+
+        // event
+        else if (message.startsWith("event")){
+            t[index] = new Event(message.substring(6, message.indexOf(" /")),
+                    message.substring(message.indexOf("/at") + 4));
+            acknowledgement(t[index], index+1);
+            index++;
+        }
+
+        else {
+            throw new DukeException();
+        }
+
+        return index;
+    }
+
     public static void acknowledgement(Task message, int i){
         System.out.println("Got it. I've added this task:");
         System.out.println("    " + message);
@@ -32,70 +84,32 @@ public class Duke {
 
 
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        greet();
-
-        Scanner in = new Scanner(System.in);
-        String line = in.nextLine();
 
         Task[] t = new Task[100];
         int index = 0;
 
+        greet();
+
+        Scanner in = new Scanner(System.in);
+        String message = in.nextLine();
+
 
         // main loop
-        while (!line.equals("bye")){
+        while (!message.equals("bye")){
 
             printLine();
-
-            // list
-            if (line.equals("list")){
-                for (int i = 0; i < index; i++){
-                    System.out.print(i+1 + ". ");
-                    System.out.println(t[i]);
-                }
+            try {
+                index = addTask(t, message, index);
             }
-
-            // mark as done
-            else if (line.startsWith("done")){
-                int l = Integer.parseInt(line.substring(5)) - 1;
-                t[l].markAsDone();
+            catch (DukeException e){
+                System.out.println("OOPS!!! I'm sorry, but I don't know what that means.");
             }
-
-            // deadline
-            else if (line.startsWith("deadline")){
-                t[index] = new Deadline(line.substring(9, line.indexOf(" /")),
-                        line.substring(line.indexOf("/by") + 4));
-                acknowledgement(t[index], index+1);
-                index++;
+            catch (StringIndexOutOfBoundsException e){
+                System.out.println("OOPS!!! The description of a " + message + " cannot be empty.");
             }
-
-            // todo
-            else if (line.startsWith("todo")){
-                t[index] = new Todo(line.substring(5));
-                acknowledgement(t[index], index+1);
-                index++;
-            }
-
-            // event
-            else if (line.startsWith("event")){
-                t[index] = new Event(line.substring(6, line.indexOf(" /")),
-                        line.substring(line.indexOf("/at") + 4));
-                acknowledgement(t[index], index+1);
-                index++;
-            }
-
-            else {
-                t[index] = new Task(line);
-                index++;
-                System.out.println("added: " + line);
-            }
-
             printLine();
-            line = in.nextLine();
+
+            message = in.nextLine();
         }
 
         bye();
