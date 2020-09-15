98ede308df1cddf9765b46aa050a22e57839f040
jusufnathanael <jusuf.n@gmail.com>
2020-09-15 23:41:05 +0800

    Level-7

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Duke {
 
@@ -25,12 +29,65 @@ public class Duke {
         printLine();
     }
 
+    private static int initialisation(ArrayList<Task> t, int index){
+
+        try {
+            index = readFile("data/data.txt", t);
+        }
+        catch (FileNotFoundException e){
+            System.out.println("File not found");
+        }
+        return index;
+    }
+
+    private static int readFile(String filePath, ArrayList<Task> t)
+            throws FileNotFoundException {
+
+        File f = new File(filePath);
+        Scanner s = new Scanner(f); // create a Scanner using the File as the source
+        System.out.println("yes");
+        int index = 0;
+
+        while (s.hasNext()) {
+
+            String temp = s.nextLine();
+            System.out.println(temp);
+            Task newTask = null;
+
+            if (temp.startsWith("[T]")){
+                newTask = new Todo(temp.substring(7));
+                t.add(newTask);
+            }
+
+            else if (temp.startsWith("[E]")){
+                newTask = new Event(temp.substring(7, temp.indexOf(" (")),
+                        temp.substring(temp.indexOf("(at:") + 5, temp.indexOf(")")));
+                t.add(newTask);
+            }
+
+            else if (temp.startsWith("[D]")){
+                newTask = new Deadline(temp.substring(7, temp.indexOf(" (")),
+                        temp.substring(temp.indexOf("(by:") + 5, temp.indexOf(")")));
+                t.add(newTask);
+            }
+
+            /*if (temp.indexOf(4) == 1){
+
+                newTask.markAsDone();
+            }*/
+            index++;
+            //s.nextLine();
+        }
+        return index;
+    }
+
+
     public static int addTask(ArrayList<Task> t, String message, int index) throws DukeException {
 
         // list
         if (message.equals("list")){
-            for (int i = 0; i < index; i++){
-                System.out.print(i+1 + ". ");
+            for (int i = 0; i < index; i++) {
+                System.out.print(i + 1 + ". ");
                 System.out.println(t.get(i));
             }
         }
@@ -76,6 +133,7 @@ public class Duke {
             throw new DukeException();
         }
 
+        prepareWriteToFile(t);
         return index;
     }
 
@@ -90,15 +148,34 @@ public class Duke {
         System.out.println("Now you have " + i + (i == 1 ? " task" : " tasks") + " in your list.");
     }
 
+    public static void prepareWriteToFile (ArrayList<Task> tasks){
+        StringBuilder message = new StringBuilder();
+        for (Task t :tasks) {
+            message.append(t).append(System.lineSeparator());
+        }
+        try {
+            writeToFile("data/data.txt", message.toString());
+        }
+        catch (IOException e){
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
+    }
+
+    public static void writeToFile(String filePath, String message) throws IOException{
+        FileWriter fw = new FileWriter(filePath);
+        fw.write(message);
+        fw.close();
+    }
+
 
     public static void main(String[] args) {
 
-        ArrayList<Task> t = new ArrayList<>();
+        greet();
 
-        //Task[] t = new Task[100];
+        ArrayList<Task> t = new ArrayList<>();
         int index = 0;
 
-        greet();
+        index = initialisation(t, index);
 
         Scanner in = new Scanner(System.in);
         String message = in.nextLine();
